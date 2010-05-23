2af11061e4ab513052c7830a57559ee49a8a07a2
liato <x@x00.us>
2010-05-23 15:41:11 +0200

    Fixed lansforsakringar, hopefully.

--- src/com/liato/bankdroid/BankLansforsakringar.java
+++ src/com/liato/bankdroid/BankLansforsakringar.java
@@ -27,8 +27,9 @@ public class BankLansforsakringar implements Bank {
 	private Banks banktype = Banks.LANSFORSAKRINGAR;
 	private Pattern reEventValidation = Pattern.compile("__EVENTVALIDATION\"\\s+value=\"([^\"]+)\"");
 	private Pattern reViewState = Pattern.compile("__VIEWSTATE\"\\s+value=\"([^\"]+)\"");
-	private Pattern reBalance = Pattern.compile("<a.+?DataTable1:(\\d+):account[^>]+>([^<]+)</a>.+?<spa.+?>([0-9 -,.]+)</span></td>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+	private Pattern reBalance = Pattern.compile("<a.+?DataTable1:(\\d+):account[^>]+>([^<]+)</a>.+?<spa.+?>.+?</span></td>.*?<spa.+?>(.+?)</span></td>", Pattern.CASE_INSENSITIVE);
 	private Pattern reToken = Pattern.compile("var\\s+token\\s*=\\s*'([^']+)'", Pattern.CASE_INSENSITIVE);
+	private Pattern reUrl = Pattern.compile("<li class=\"bank\">\\s*<a href=\"([^\"]+)\"", Pattern.CASE_INSENSITIVE);
 	
 	private ArrayList<Account> accounts = new ArrayList<Account>();
 	private BigDecimal balance = new BigDecimal(0);
@@ -92,7 +93,12 @@ public class BankLansforsakringar implements Bank {
 			}
 			String token = matcher.group(1);
 
-			response = urlopen.open("https://secure246.lansforsakringar.se/lfportal/appmanager/privat/main?_nfpb=true&_pageLabel=bank_konto&newUc=true&isTopLevel=true&_token="+token);
+			matcher = reUrl.matcher(response);
+			if (!matcher.find()) {
+				throw new BankException(res.getText(R.string.unable_to_find).toString()+" accounts url.");
+			}
+			String accurl = Html.fromHtml(matcher.group(1)).toString(); 
+			urlopen.open(accurl+"&_token="+token);
 			matcher = reBalance.matcher(response);
 			while (matcher.find()) {
 				accounts.add(new Account(Html.fromHtml(matcher.group(2)).toString().trim(), Helpers.parseBalance(matcher.group(3).trim()), matcher.group(1).trim()));
