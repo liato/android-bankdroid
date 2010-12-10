9a44db6b4530f5faa5a14045b553fc90cc4e2ff4
liato <x@x00.us>
2010-12-10 15:22:43 +0100

    Added currency parser.

--- src/com/liato/bankdroid/Helpers.java
+++ src/com/liato/bankdroid/Helpers.java
@@ -9,6 +9,52 @@ import android.app.Activity;
 import android.util.Log;
 
 public class Helpers {
+    private final static String[] currencies = {"AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD",
+                                         "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF",
+                                         "BMD", "BND", "BOB", "BRL", "BSD", "BTN", "BWP", "BYR",
+                                         "BZD", "CAD", "CDF", "CHF", "CLP", "CNY", "COP", "CRC",
+                                         "CUP", "CVE", "CYP", "CZK", "DJF", "DKK", "DOP", "DZD",
+                                         "EEK", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "GBP",
+                                         "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD",
+                                         "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP",
+                                         "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY",
+                                         "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD",
+                                         "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LTL", "LVL",
+                                         "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP",
+                                         "MRO", "MTL", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN",
+                                         "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB",
+                                         "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON",
+                                         "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK",
+                                         "SGD", "SHP", "SLL", "SOS", "SPL", "SRD", "STD", "SVC",
+                                         "SYP", "SZL", "THB", "TJS", "TMM", "TND", "TOP", "TRY",
+                                         "TTD", "TVD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU",
+                                         "UZS", "VEB", "VEF", "VND", "VUV", "WST", "XAF", "XAG",
+                                         "XAU", "XCD", "XDR", "XOF", "XPD", "XPF", "XPT", "YER",
+                                         "ZAR", "ZMK", "ZWD"};
+
+    private final static String[][] symMappings = {{"$U", "UYU"}, {"$b", "BOB"}, {"BZ$", "BZD"},
+                                            {"C$", "NIO"}, {"J$", "JMD"}, {"NT$", "TWD"},
+                                            {"R$", "BRL"}, {"RD$", "DOP"}, {"TT$", "TTD"},
+                                            {"Z$", "ZWD"}, {"$", "USD"}, {"B/.", "PAB"},
+                                            {"Bs", "VEF"}, {"Ft", "HUF"}, {"Gs", "PYG"},
+                                            {"KM", "BAM"}, {"Kč", "CZK"}, {"Lek", "ALL"},
+                                            {"S/.", "PEN"}, {"Ls", "LVL"}, {"Lt", "LTL"},
+                                            {"MT", "MZN"}, {"Php", "PHP"}, {"RM", "MYR"},
+                                            {"Rp", "IDR"}, {"TL", "TRY"}, {"kn", "HRK"},
+                                            {"kr", "SEK"}, {"lei", "RON"}, {"p.", "BYR"},
+                                            {"L", "HNL"}, {"S", "SOS"}, {"P", "BWP"},
+                                            {"Q", "GTQ"}, {"R", "ZAR"}, {"zł", "PLN"},
+                                            {"¢", "GHC"}, {"£", "GBP"}, {"¥", "JPY"},
+                                            {"ƒ", "ANG"}, {"Дин.", "RSD"}, {"ден", "MKD"},
+                                            {"лв", "BGN"}, {"ман", "AZN"}, {"руб", "RUB"},
+                                            {"؋", "AFN"}, {"฿", "THB"}, {"៛", "KHR"},
+                                            {"₡", "CRC"}, {"₤", "TRL"}, {"₦", "NGN"},
+                                            {"₨", "PKR"}, {"₩", "KRW"}, {"₪", "ILS"},
+                                            {"₫", "VND"}, {"€", "EUR"}, {"₭", "LAK"},
+                                            {"₮", "MNT"}, {"₱", "CUP"}, {"₴", "UAH"},
+                                            {"﷼", "SAR"}, 
+}; 
+    
 	public static BigDecimal parseBalance(String balance) {
 		//balance = balance.replaceAll("(?:\\.|&nbsp;| )*", "");
 		//balance = balance.replaceAll("[a-zA-Z]*", "");
@@ -62,6 +108,16 @@ public class Helpers {
 	    } catch (Exception e) {
 	        // Can't change animation, so do nothing
 	    }
+	}
+	
+	public static String parseCurrency(String text, String def) {
+	    for (String currency : currencies) {
+	        if (text.contains(currency)) return currency;
+	    }
+        for (String[] symCur : symMappings) {
+            if (text.contains(symCur[0])) return symCur[1];
+        }
+	    return def;
 	}
 	
 }
