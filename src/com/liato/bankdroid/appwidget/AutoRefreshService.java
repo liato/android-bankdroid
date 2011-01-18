2248defe87e22d09bf09bc9e72c9a1f22665dd73
Magnus Andersson <magnus@magnusart.com>
2011-01-18 22:29:34 +0100

    Added a broadcast when transactions have been updated. Right now it sends all the accounts because of the semantics in the application of clearing all the transactions for each account.
    Change-Id: I589a2a0dae5e079f37b8efb9b1619246d7152c70

--- src/com/liato/bankdroid/appwidget/AutoRefreshService.java
+++ src/com/liato/bankdroid/appwidget/AutoRefreshService.java
@@ -20,23 +20,11 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import com.liato.bankdroid.Helpers;
-import com.liato.bankdroid.MainActivity;
-import com.liato.bankdroid.R;
-import com.liato.bankdroid.R.id;
-import com.liato.bankdroid.R.string;
-import com.liato.bankdroid.banking.Account;
-import com.liato.bankdroid.banking.Bank;
-import com.liato.bankdroid.banking.BankFactory;
-import com.liato.bankdroid.banking.exceptions.BankException;
-import com.liato.bankdroid.banking.exceptions.LoginException;
-import com.liato.bankdroid.db.DBAdapter;
-
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.app.PendingIntent.CanceledException;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -47,6 +35,16 @@ import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
+import com.liato.bankdroid.Helpers;
+import com.liato.bankdroid.MainActivity;
+import com.liato.bankdroid.R;
+import com.liato.bankdroid.banking.Account;
+import com.liato.bankdroid.banking.Bank;
+import com.liato.bankdroid.banking.BankFactory;
+import com.liato.bankdroid.banking.exceptions.BankException;
+import com.liato.bankdroid.banking.exceptions.LoginException;
+import com.liato.bankdroid.db.DBAdapter;
+
 public class AutoRefreshService extends Service {
 	private final static String TAG = "AutoRefreshService";
 	public final static String BROADCAST_WIDGET_REFRESH = "com.liato.bankdroid.WIDGET_REFRESH";
@@ -55,213 +53,275 @@ public class AutoRefreshService extends Service {
 	public final static String BROADCAST_OPENWATCH_TEXT = "com.smartmadsoft.openwatch.action.TEXT";
 	public final static String BROADCAST_OPENWATCH_VIBRATE = "com.smartmadsoft.openwatch.action.VIBRATE";
 	public final static String ACTION_MAIN_SHOW_TRANSACTIONS = "com.liato.bankdroid.action.MAIN_SHOW_TRANSACTIONS";
-    
-    NotificationManager notificationManager;
+	public final static String BROADCAST_TRANSACTIONS_UPDATED = "com.liato.bankdroid.action.TRANSACTIONS";
 
-    @Override
-    public void onCreate() {
-        notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-        new DataRetrieverTask().execute();
-    }
+	NotificationManager notificationManager;
 
-    @Override
-    public void onDestroy() {
-    }
+	@Override
+	public void onCreate() {
+		notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		new DataRetrieverTask().execute();
+	}
 
+	@Override
+	public void onDestroy() {
+	}
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
+	@Override
+	public IBinder onBind(final Intent intent) {
+		return null;
+	}
 
-    private void showNotification(String text, int icon, String title, String bank) {
-    	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		if (!prefs.getBoolean("notify_on_change", true)) return;
-		
-        Notification notification = new Notification(icon, text,
-                System.currentTimeMillis());
-        // Remove notification from statusbar when clicked
-        notification.flags |= Notification.FLAG_AUTO_CANCEL;
-        
-        
-        //http://www.freesound.org/samplesViewSingle.php?id=75235
-        //http://www.freesound.org/samplesViewSingle.php?id=91924
-        Log.d(TAG, "Notification sound: "+prefs.getString("notification_sound", "none"));
-        if (prefs.getString("notification_sound", null) != null) {
-            notification.sound = Uri.parse(prefs.getString("notification_sound", null));
-        }
-		if (prefs.getBoolean("notify_with_vibration", true)) {
-			long[] vib = {0, 90, 130, 80, 350, 190, 20, 380};
-			notification.vibrate = vib;
-			//notification.defaults |= Notification.DEFAULT_VIBRATE;
+	private void showNotification(final String text, final int icon,
+			final String title, final String bank) {
+		final SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		if (!prefs.getBoolean("notify_on_change", true)) {
+			return;
 		}
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, MainActivity.class), 0);
 
-        notification.setLatestEventInfo(this, title, text, contentIntent);
+		final Notification notification = new Notification(icon, text,
+				System.currentTimeMillis());
+		// Remove notification from statusbar when clicked
+		notification.flags |= Notification.FLAG_AUTO_CANCEL;
 
-        notificationManager.notify(R.id.about, notification);
-        
-        // Broadcast to Remote Notifier if enabled
-        // http://code.google.com/p/android-notifier/
-        if (prefs.getBoolean("notify_remotenotifier", false)) {
-            Intent i = new Intent(BROADCAST_REMOTE_NOTIFIER);
-            i.putExtra("title", String.format("%s (%s)", bank, title));
-            i.putExtra("description", text);
-            sendBroadcast(i);
-        }
+		// http://www.freesound.org/samplesViewSingle.php?id=75235
+		// http://www.freesound.org/samplesViewSingle.php?id=91924
+		Log.d(TAG,
+				"Notification sound: "
+						+ prefs.getString("notification_sound", "none"));
+		if (prefs.getString("notification_sound", null) != null) {
+			notification.sound = Uri.parse(prefs.getString(
+					"notification_sound", null));
+		}
+		if (prefs.getBoolean("notify_with_vibration", true)) {
+			final long[] vib = { 0, 90, 130, 80, 350, 190, 20, 380 };
+			notification.vibrate = vib;
+			// notification.defaults |= Notification.DEFAULT_VIBRATE;
+		}
+		final PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+				new Intent(this, MainActivity.class), 0);
 
-        // Broadcast to OpenWatch if enabled
-        // http://forum.xda-developers.com/showthread.php?t=554551
-        if (prefs.getBoolean("notify_openwatch", false)) {
-            Intent i;
-            if (prefs.getBoolean("notify_openwatch_vibrate", false)) {
-                i = new Intent(BROADCAST_OPENWATCH_VIBRATE);
-            }
-            else {
-                i = new Intent(BROADCAST_OPENWATCH_TEXT);
-            }
-            i.putExtra("line1", String.format("%s (%s)", bank, title));
-            i.putExtra("line2", text);
-            sendBroadcast(i);
-        }
-        
-        
-    }
-    
-    private class DataRetrieverTask extends AsyncTask<String, String, Void> {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(AutoRefreshService.this);
-    	private ArrayList<String> errors;
-    	private Resources res;
+		notification.setLatestEventInfo(this, title, text, contentIntent);
 
-    	public DataRetrieverTask() {
-    	}
-    	protected void onPreExecute() {
-    	}
+		notificationManager.notify(R.id.about, notification);
 
-    	protected Void doInBackground(final String... args) {
-    		errors = new ArrayList<String>();
-    		Boolean refreshWidgets = false;
-    		ArrayList<Bank> banks = BankFactory.banksFromDb(AutoRefreshService.this, true);
-    		if (banks.isEmpty()) {
-    			return null;
-    		}
-    		DBAdapter db = new DBAdapter(AutoRefreshService.this);
-    		db.open();    		
-    		BigDecimal currentBalance;
-    		BigDecimal diff;
-    		HashMap<String, Account> accounts = new HashMap<String, Account>();
-    		
-    		for (Bank bank : banks) {
-                if (prefs.getBoolean("debug_mode", false) && prefs.getBoolean("debug_only_testbank", false)) {
-                    Log.d(TAG, "Debug::Only_Testbank is ON. Skipping update for "+bank.getName());
-                    continue;
-                }
-    			if (bank.isDisabled()) {
-    				Log.d(TAG, bank.getName()+" ("+bank.getDisplayName()+") is disabled. Skipping refresh.");
-    				continue;
-    			}
-				Log.d(TAG, "Refreshing "+bank.getName()+" ("+bank.getDisplayName()+").");
-    			try {
-    				currentBalance = bank.getBalance();
-    				accounts.clear();
-    				for(Account account : bank.getAccounts()) {
-    					accounts.put(account.getId(), account);
-    				}
-    				bank.update();
-					diff = currentBalance.subtract(bank.getBalance());
-    				if (diff.compareTo(new BigDecimal(0)) != 0) {
-    					Account oldAccount;
-    					for(Account account : bank.getAccounts()) {
-    						oldAccount = accounts.get(account.getId());
-    						if (oldAccount != null) {
-    							if (account.getBalance().compareTo(oldAccount.getBalance()) != 0) {
-    							    boolean notify = false;
-    							    switch (account.getType()) {
-    							    case Account.REGULAR:
-    							        notify = prefs.getBoolean("notify_for_deposit", true);
-    							        break;
-    							    case Account.FUNDS:
-    							        notify = prefs.getBoolean("notify_for_funds", false);
-    							        break;
-    							    case Account.LOANS:
-    							        notify = prefs.getBoolean("notify_for_loans", false);
-    							        break;
-    							    case Account.CCARD:
-    							        notify = prefs.getBoolean("notify_for_ccards", true);
-    							        break;
-    							    case Account.OTHER:
-    							        notify = prefs.getBoolean("notify_for_other", false);
-    							        break;
-    							    }
-    							    Log.d(TAG, "Account type: "+account.getType()+"; notify: "+notify);
-    							    if (account.isHidden() || !account.isNotify()) {
-    							        notify = false;
-    							    }
-    		                        if (notify) {
-    		                            diff = account.getBalance().subtract(oldAccount.getBalance());
-        								showNotification(account.getName()+ ": "+ ((diff.compareTo(new BigDecimal(0)) == 1) ? "+" : "") + Helpers.formatBalance(diff, account.getCurrency()) + " ("+Helpers.formatBalance(account.getBalance(), account.getCurrency())+")",
-        												 bank.getImageResource(), bank.getDisplayName(), bank.getName());
-                                    }
-    								refreshWidgets = true;
-    							}
-    						}
-    					}
-    					if (prefs.getBoolean("autoupdates_transactions_enabled", true)) {
-    					    bank.updateAllTransactions();    					    
-    					}
-    				}
-    				bank.closeConnection();
-    				db.updateBank(bank);
-    			} 
-    			catch (BankException e) {
-    				// Refresh widgets if an update fails
-    				Log.d(TAG, "Error while updating bank '"+bank.getDbId()+"'; BankException: "+e.getMessage());
-    			} catch (LoginException e) {
-                    Log.d(TAG, "Error while updating bank '"+bank.getDbId()+"'; LoginException: "+e.getMessage());
-    				refreshWidgets = true;
-    				db.disableBank(bank.getDbId());
+		// Broadcast to Remote Notifier if enabled
+		// http://code.google.com/p/android-notifier/
+		if (prefs.getBoolean("notify_remotenotifier", false)) {
+			final Intent i = new Intent(BROADCAST_REMOTE_NOTIFIER);
+			i.putExtra("title", String.format("%s (%s)", bank, title));
+			i.putExtra("description", text);
+			sendBroadcast(i);
+		}
+
+		// Broadcast to OpenWatch if enabled
+		// http://forum.xda-developers.com/showthread.php?t=554551
+		if (prefs.getBoolean("notify_openwatch", false)) {
+			Intent i;
+			if (prefs.getBoolean("notify_openwatch_vibrate", false)) {
+				i = new Intent(BROADCAST_OPENWATCH_VIBRATE);
+			} else {
+				i = new Intent(BROADCAST_OPENWATCH_TEXT);
+			}
+			i.putExtra("line1", String.format("%s (%s)", bank, title));
+			i.putExtra("line2", text);
+			sendBroadcast(i);
+		}
+
+	}
+
+	private class DataRetrieverTask extends AsyncTask<String, String, Void> {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(AutoRefreshService.this);
+		private ArrayList<String> errors;
+		private Resources res;
+
+		public DataRetrieverTask() {
+		}
+
+		@Override
+		protected void onPreExecute() {
+		}
+
+		@Override
+		protected Void doInBackground(final String... args) {
+			errors = new ArrayList<String>();
+			Boolean refreshWidgets = false;
+			final ArrayList<Bank> banks = BankFactory.banksFromDb(
+					AutoRefreshService.this, true);
+			if (banks.isEmpty()) {
+				return null;
+			}
+			final DBAdapter db = new DBAdapter(AutoRefreshService.this);
+			db.open();
+			BigDecimal currentBalance;
+			BigDecimal diff;
+			final HashMap<String, Account> accounts = new HashMap<String, Account>();
+
+			for (final Bank bank : banks) {
+				if (prefs.getBoolean("debug_mode", false)
+						&& prefs.getBoolean("debug_only_testbank", false)) {
+					Log.d(TAG,
+							"Debug::Only_Testbank is ON. Skipping update for "
+									+ bank.getName());
+					continue;
 				}
-    		}
-    		
+				if (bank.isDisabled()) {
+					Log.d(TAG, bank.getName() + " (" + bank.getDisplayName()
+							+ ") is disabled. Skipping refresh.");
+					continue;
+				}
+				Log.d(TAG,
+						"Refreshing " + bank.getName() + " ("
+								+ bank.getDisplayName() + ").");
+				try {
+					currentBalance = bank.getBalance();
+					accounts.clear();
+					for (final Account account : bank.getAccounts()) {
+						accounts.put(account.getId(), account);
+					}
+					bank.update();
+					diff = currentBalance.subtract(bank.getBalance());
+					if (diff.compareTo(new BigDecimal(0)) != 0) {
+						Account oldAccount;
+						for (final Account account : bank.getAccounts()) {
+							oldAccount = accounts.get(account.getId());
+							if (oldAccount != null) {
+								if (account.getBalance().compareTo(
+										oldAccount.getBalance()) != 0) {
+									boolean notify = false;
+									switch (account.getType()) {
+									case Account.REGULAR:
+										notify = prefs.getBoolean(
+												"notify_for_deposit", true);
+										break;
+									case Account.FUNDS:
+										notify = prefs.getBoolean(
+												"notify_for_funds", false);
+										break;
+									case Account.LOANS:
+										notify = prefs.getBoolean(
+												"notify_for_loans", false);
+										break;
+									case Account.CCARD:
+										notify = prefs.getBoolean(
+												"notify_for_ccards", true);
+										break;
+									case Account.OTHER:
+										notify = prefs.getBoolean(
+												"notify_for_other", false);
+										break;
+									}
+									Log.d(TAG,
+											"Account type: "
+													+ account.getType()
+													+ "; notify: " + notify);
+									if (account.isHidden()
+											|| !account.isNotify()) {
+										notify = false;
+									}
+									if (notify) {
+										diff = account.getBalance().subtract(
+												oldAccount.getBalance());
+										showNotification(
+												account.getName()
+														+ ": "
+														+ ((diff.compareTo(new BigDecimal(
+																0)) == 1) ? "+"
+																: "")
+														+ Helpers
+																.formatBalance(
+																		diff,
+																		account.getCurrency())
+														+ " ("
+														+ Helpers
+																.formatBalance(
+																		account.getBalance(),
+																		account.getCurrency())
+														+ ")",
+												bank.getImageResource(),
+												bank.getDisplayName(),
+												bank.getName());
+									}
+
+									refreshWidgets = true;
+								}
+							}
+						}
+						if (prefs.getBoolean(
+								"autoupdates_transactions_enabled", true)) {
+							bank.updateAllTransactions();
+						}
+					}
+					bank.closeConnection();
+					db.updateBank(bank);
+				} catch (final BankException e) {
+					// Refresh widgets if an update fails
+					Log.d(TAG, "Error while updating bank '" + bank.getDbId()
+							+ "'; BankException: " + e.getMessage());
+				} catch (final LoginException e) {
+					Log.d(TAG, "Error while updating bank '" + bank.getDbId()
+							+ "'; LoginException: " + e.getMessage());
+					refreshWidgets = true;
+					db.disableBank(bank.getDbId());
+				}
+			}
+
+			// Sending updates for all accounts, because we overwrite everything
+			// at this point.
+			if (prefs.getBoolean("content_provider_enabled", false)) {
+				for (final Account account : accounts.values()) {
+					broadcastTransactionUpdate(getBaseContext(), account);
+				}
+			}
+
 			if (refreshWidgets) {
-				Intent updateIntent = new Intent(BROADCAST_MAIN_REFRESH);
+				final Intent updateIntent = new Intent(BROADCAST_MAIN_REFRESH);
 				sendBroadcast(updateIntent);
 				sendWidgetRefresh(AutoRefreshService.this);
 			}
-    		db.close();
-    		return null;
-    	}
+			db.close();
+			return null;
+		}
 
-    	protected void onProgressUpdate(String... args) {
-    	}
-    	protected void onPostExecute(final Void unused) {
-    		if (this.errors != null && !this.errors.isEmpty()) {
-    			StringBuilder errormsg = new StringBuilder();
-    			errormsg.append(res.getText(R.string.accounts_were_not_updated)+":\n");
-    			for (String err : errors)
-    			{
-    			  errormsg.append(err);
-    			  errormsg.append("\n");
-    			}
-    			Log.d("aa", errormsg.toString());
-    		}
-    		AutoRefreshService.this.stopSelf();
-    	}
-    }	
+		@Override
+		protected void onProgressUpdate(final String... args) {
+		}
 
-    
-    public static void sendWidgetRefresh(Context context) {
-    	//Send intent to BankdroidWidgetProvider
-        Intent updateIntent = new Intent(BROADCAST_WIDGET_REFRESH);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(
-        		context, 0, updateIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-            try {
-				pendingIntent.send();
-			} catch (CanceledException e) {
-				// TODO Auto-generated catch block
-				Log.e("", e.getMessage(), e);
+		@Override
+		protected void onPostExecute(final Void unused) {
+			if ((this.errors != null) && !this.errors.isEmpty()) {
+				final StringBuilder errormsg = new StringBuilder();
+				errormsg.append(res.getText(R.string.accounts_were_not_updated)
+						+ ":\n");
+				for (final String err : errors) {
+					errormsg.append(err);
+					errormsg.append("\n");
+				}
+				Log.d("aa", errormsg.toString());
 			}
-    }    
+			AutoRefreshService.this.stopSelf();
+		}
+	}
+
+	public static void broadcastTransactionUpdate(final Context context,
+			final Account account) {
+		final Intent i = new Intent(BROADCAST_TRANSACTIONS_UPDATED);
+		i.putExtra("accountId", account.getId());
+		context.sendBroadcast(i);
+	}
+
+	public static void sendWidgetRefresh(final Context context) {
+		// Send intent to BankdroidWidgetProvider
+		final Intent updateIntent = new Intent(BROADCAST_WIDGET_REFRESH);
+		final PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+				0, updateIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+		try {
+			pendingIntent.send();
+		} catch (final CanceledException e) {
+			// TODO Auto-generated catch block
+			Log.e("", e.getMessage(), e);
+		}
+	}
 }
