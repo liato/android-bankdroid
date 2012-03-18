f0afdf4eee4a46d68d9ce8f0b9d16f601bc1bfa2
Peter Björkman <peter@peterbjorkman.se>
2012-03-18 17:47:19 +0100

    Remove unused classes in the se.smartrefill namespace

--- src/se/smartrefill/remoting/domain/exception/NoSessionException.java
+++
@@ -1,8 +0,0 @@
-package se.smartrefill.remoting.domain.exception;
-
-public class NoSessionException extends SmartrefillException {
-	private static final long serialVersionUID = 1L;
-
-	public NoSessionException() {
-	}
-}
