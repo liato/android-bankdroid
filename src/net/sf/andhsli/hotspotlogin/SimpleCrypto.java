1099740817262507306e2f57dc67c218fa99f312
Jacob Siverskog <jacob.siverskog#gh@gmail.com>
2012-11-18 21:40:47 +0100

    Fix update glitch for Android 4.2. Thanks to gfu (http://www.swedroid.se/forum/showthread.php?t=11108&page=219). Tested with 4.2 and 4.1.

--- src/net/sf/andhsli/hotspotlogin/SimpleCrypto.java
+++ src/net/sf/andhsli/hotspotlogin/SimpleCrypto.java
@@ -38,7 +38,7 @@ public class SimpleCrypto {
 
 	private static byte[] getRawKey(byte[] seed) throws Exception {
 		KeyGenerator kgen = KeyGenerator.getInstance("AES");
-		SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+		SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "Crypto");
 		sr.setSeed(seed);
 	    kgen.init(128, sr); // 192 and 256 bits may not be available
 	    SecretKey skey = kgen.generateKey();
