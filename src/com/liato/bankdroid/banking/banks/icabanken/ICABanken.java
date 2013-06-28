7f5df9ba69598b39ecffaee2182b6f2bc735a3d5
Mathias Åhsberg <mathias.ahsberg@gmail.com>
2013-06-28 18:10:56 +0200

    Fixes formatting

--- src/com/liato/bankdroid/banking/banks/icabanken/ICABanken.java
+++ src/com/liato/bankdroid/banking/banks/icabanken/ICABanken.java
@@ -20,19 +20,14 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.http.client.ClientProtocolException;
 
 import android.content.Context;
-import android.text.Html;
 import android.text.InputType;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.liato.bankdroid.Helpers;
 import com.liato.bankdroid.R;
 import com.liato.bankdroid.banking.Account;
 import com.liato.bankdroid.banking.Bank;
@@ -49,56 +44,56 @@ import com.liato.bankdroid.provider.IBankTypes;
 import eu.nullbyte.android.urllib.Urllib;
 
 public class ICABanken extends Bank {
-    private static final String TAG = "ICABanken";
-    private static final String NAME = "ICA Banken";
-    private static final String NAME_SHORT = "icabanken";
-    private static final String URL = "https://mobil.icabanken.se/";
-    private static final int BANKTYPE_ID = IBankTypes.ICABANKEN;
-    private static final int INPUT_TYPE_USERNAME = InputType.TYPE_CLASS_PHONE;
-    private static final int INPUT_TYPE_PASSWORD = InputType.TYPE_CLASS_PHONE;
-    private static final String INPUT_HINT_USERNAME = "ÅÅÅÅMMDD-XXXX";
-    private static final boolean STATIC_BALANCE = false;
+	private static final String TAG = "ICABanken";
+	private static final String NAME = "ICA Banken";
+	private static final String NAME_SHORT = "icabanken";
+	private static final String URL = "https://mobil.icabanken.se/";
+	private static final int BANKTYPE_ID = IBankTypes.ICABANKEN;
+	private static final int INPUT_TYPE_USERNAME = InputType.TYPE_CLASS_PHONE;
+	private static final int INPUT_TYPE_PASSWORD = InputType.TYPE_CLASS_PHONE;
+	private static final String INPUT_HINT_USERNAME = "ÅÅÅÅMMDD-XXXX";
+	private static final boolean STATIC_BALANCE = false;
 
-    private static final String API_KEY = "A026CF97-5F6A-438D-A921-AA3406A6CE09";
-    private static final String API_URL = "https://appserver.icabanken.se";
-    
-    private Pattern reTransactions = Pattern.compile("<label>(.+?)</label>\\s*<[^>]+(.+?)</div>\\s*<[^>]+>-\\s*Belopp(.+?)<", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-    private HashMap<String, String> mIdMappings = new HashMap<String, String>();
+	private static final String API_KEY = "A026CF97-5F6A-438D-A921-AA3406A6CE09";
+	private static final String API_URL = "https://appserver.icabanken.se";
 
-    public ICABanken(Context context) {
-        super(context);
-        super.TAG = TAG;
-        super.NAME = NAME;
-        super.NAME_SHORT = NAME_SHORT;
-        super.BANKTYPE_ID = BANKTYPE_ID;
-        super.URL = URL;
-        super.INPUT_TYPE_USERNAME = INPUT_TYPE_USERNAME;
-        super.INPUT_TYPE_PASSWORD = INPUT_TYPE_PASSWORD;
-        super.INPUT_HINT_USERNAME = INPUT_HINT_USERNAME;
-        super.STATIC_BALANCE = STATIC_BALANCE;
-    }
+	public ICABanken(Context context) {
+		super(context);
+		super.TAG = TAG;
+		super.NAME = NAME;
+		super.NAME_SHORT = NAME_SHORT;
+		super.BANKTYPE_ID = BANKTYPE_ID;
+		super.URL = URL;
+		super.INPUT_TYPE_USERNAME = INPUT_TYPE_USERNAME;
+		super.INPUT_TYPE_PASSWORD = INPUT_TYPE_PASSWORD;
+		super.INPUT_HINT_USERNAME = INPUT_HINT_USERNAME;
+		super.STATIC_BALANCE = STATIC_BALANCE;
+	}
 
-    public ICABanken(String username, String password, Context context) throws BankException, LoginException, BankChoiceException {
-        this(context);
-        this.update(username, password);
-    }
+	public ICABanken(String username, String password, Context context)
+			throws BankException, LoginException, BankChoiceException {
+		this(context);
+		this.update(username, password);
+	}
 
-    
-   public Urllib login() throws LoginException, BankException {
-        urlopen = new Urllib();
-        urlopen.addHeader("ApiVersion", "1.0");
-        urlopen.addHeader("Accept", "application/json");
-        urlopen.addHeader("ApiKey", API_KEY);
-        String response = null;
-        try {
-		response = urlopen.open(API_URL+"/login?customerId="+username+"&password="+password);
-		
-		ObjectMapper vObjectMapper = new ObjectMapper();
-		vObjectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss",new Locale("sv","SE")));
-		LoginResponse loginResponse = vObjectMapper.readValue(response, LoginResponse.class);
-		
-		addAccounts(loginResponse.getAccountList());
-		
+	public Urllib login() throws LoginException, BankException {
+		urlopen = new Urllib();
+		urlopen.addHeader("ApiVersion", "1.0");
+		urlopen.addHeader("Accept", "application/json");
+		urlopen.addHeader("ApiKey", API_KEY);
+		String response = null;
+		try {
+			response = urlopen.open(API_URL + "/login?customerId=" + username
+					+ "&password=" + password);
+
+			ObjectMapper vObjectMapper = new ObjectMapper();
+			vObjectMapper.setDateFormat(new SimpleDateFormat(
+					"yyyy-MM-dd hh:mm:ss", new Locale("sv", "SE")));
+			LoginResponse loginResponse = vObjectMapper.readValue(response,
+					LoginResponse.class);
+
+			addAccounts(loginResponse.getAccountList());
+
 		} catch (ClientProtocolException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -106,48 +101,59 @@ public class ICABanken extends Bank {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        return urlopen;
-    }    
-    
-    @Override
-    public void update() throws BankException, LoginException, BankChoiceException {
-        super.update();
-        if (username == null || password == null || username.length() == 0 || password.length() == 0) {
-            throw new LoginException(res.getText(R.string.invalid_username_password).toString());
-        }
-             login();
-           
-            if (accounts.isEmpty()) {
-                throw new BankException(res.getText(R.string.no_accounts_found).toString());
-            }
-        
-    }
+		return urlopen;
+	}
 
-    @Override
-    public void updateTransactions(Account account, Urllib urlopen) throws LoginException, BankException {
-        super.updateTransactions(account, urlopen);
-            super.updateComplete();
-    }
+	@Override
+	public void update() throws BankException, LoginException,
+			BankChoiceException {
+		super.update();
+		if (username == null || password == null || username.length() == 0
+				|| password.length() == 0) {
+			throw new LoginException(res.getText(
+					R.string.invalid_username_password).toString());
+		}
+		login();
 
-private void addAccounts(IcaBankenAccountList pAccountList) {
-	for(IcaBankenAccount icaAccount : pAccountList.getAccounts()) {
-		Account account = new Account(icaAccount.getName()+" (Disponibelt)",icaAccount.getAvailableAmount(),icaAccount.getAccountId());
-		account.setTransactions(mapTransactions(icaAccount));
-		Account alias = new Account(icaAccount.getName() + " (Saldo)",icaAccount.getCurrentAmount(),"a:" +icaAccount.getAccountId());
-		alias.setAliasfor(icaAccount.getAccountId());
-		accounts.add(account);		
-		accounts.add(alias);
+		if (accounts.isEmpty()) {
+			throw new BankException(res.getText(R.string.no_accounts_found)
+					.toString());
+		}
+
+	}
+
+	@Override
+	public void updateTransactions(Account account, Urllib urlopen)
+			throws LoginException, BankException {
+		super.updateTransactions(account, urlopen);
+		super.updateComplete();
+	}
+
+	private void addAccounts(IcaBankenAccountList pAccountList) {
+		for (IcaBankenAccount icaAccount : pAccountList.getAccounts()) {
+			Account account = new Account(icaAccount.getName()
+					+ " (Disponibelt)", icaAccount.getAvailableAmount(),
+					icaAccount.getAccountId());
+			account.setTransactions(mapTransactions(icaAccount));
+			Account alias = new Account(icaAccount.getName() + " (Saldo)",
+					icaAccount.getCurrentAmount(), "a:"
+							+ icaAccount.getAccountId());
+			alias.setAliasfor(icaAccount.getAccountId());
+			accounts.add(account);
+			accounts.add(alias);
+		}
+	}
+
+	private ArrayList<Transaction> mapTransactions(IcaBankenAccount pAccount) {
+		ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd",
+				Locale.getDefault());
+		for (IcaBankenTransaction icaTransaction : pAccount.getTransactions()) {
+			String date = dateFormatter.format(icaTransaction.getPostedDate());
+			Transaction transaction = new Transaction(date,
+					icaTransaction.getMemoText(), icaTransaction.getAmount());
+			transactions.add(transaction);
+		}
+		return transactions;
 	}
 }
-private ArrayList<Transaction> mapTransactions(IcaBankenAccount pAccount) {
-	ArrayList<Transaction> transactions = new ArrayList<Transaction>();
-	DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd",Locale.getDefault());
-	for(IcaBankenTransaction icaTransaction : pAccount.getTransactions()) {
-		String date = dateFormatter.format(icaTransaction.getPostedDate());
-		Transaction transaction = new Transaction(date,icaTransaction.getMemoText(),icaTransaction.getAmount());
-		transactions.add(transaction);
-	}
-	return transactions;
-}
-}
-
