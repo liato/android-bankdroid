48c01d8691cc293c77ddd8bb22de63ff66091079
liato <x@x00.us>
2014-01-12 23:33:19 +0100

    Fix for Rikslunchen.

--- src/com/liato/bankdroid/banking/banks/Rikslunchen.java
+++ src/com/liato/bankdroid/banking/banks/Rikslunchen.java
@@ -16,12 +16,18 @@
  */
 package com.liato.bankdroid.banking.banks;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import android.content.Context;
+import android.text.InputType;
+import android.text.TextUtils;
+
+import com.liato.bankdroid.Helpers;
+import com.liato.bankdroid.R;
+import com.liato.bankdroid.banking.Account;
+import com.liato.bankdroid.banking.Bank;
+import com.liato.bankdroid.banking.banks.rikslunchen.model.Envelope;
+import com.liato.bankdroid.banking.exceptions.BankChoiceException;
+import com.liato.bankdroid.banking.exceptions.BankException;
+import com.liato.bankdroid.banking.exceptions.LoginException;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
@@ -33,23 +39,22 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.ClientContext;
 import org.apache.http.cookie.Cookie;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
 
-import android.content.Context;
-import android.text.InputType;
-import android.util.Log;
-
-import com.liato.bankdroid.Helpers;
-import com.liato.bankdroid.R;
-import com.liato.bankdroid.banking.Account;
-import com.liato.bankdroid.banking.Bank;
-import com.liato.bankdroid.banking.exceptions.BankChoiceException;
-import com.liato.bankdroid.banking.exceptions.BankException;
-import com.liato.bankdroid.banking.exceptions.LoginException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import eu.nullbyte.android.urllib.CertificateReader;
 import eu.nullbyte.android.urllib.Urllib;
@@ -61,9 +66,6 @@ public class Rikslunchen extends Bank {
 	private static final String NAME_SHORT = "rikslunchen";
 	private static final String URL = "http://www.rikslunchen.se/index.html";
 	private static final int BANKTYPE_ID = Bank.RIKSLUNCHEN;
-	private static final int INPUT_TYPE_PASSWORD = InputType.TYPE_CLASS_PHONE;
-    protected boolean INPUT_HIDDEN_USERNAME = true;
-    protected int INPUT_TITLETEXT_PASSWORD = R.string.card_id;	
 
 	private String myResponse = "";
 
@@ -74,10 +76,9 @@ public class Rikslunchen extends Bank {
 		super.NAME_SHORT = NAME_SHORT;
 		super.BANKTYPE_ID = BANKTYPE_ID;
 		super.URL = URL;
-		super.INPUT_HINT_USERNAME = "Rikslunchen";
-		super.INPUT_TYPE_PASSWORD = INPUT_TYPE_PASSWORD;
-		super.INPUT_HIDDEN_USERNAME = INPUT_HIDDEN_USERNAME;
-		super.INPUT_TITLETEXT_PASSWORD = INPUT_TITLETEXT_PASSWORD;
+		super.INPUT_TYPE_USERNAME = InputType.TYPE_CLASS_PHONE;
+		super.INPUT_TITLETEXT_USERNAME = R.string.card_id;
+        super.INPUT_HIDDEN_PASSWORD = true;
 	}
 
 	public Rikslunchen(String username, String password, Context context) throws BankException, LoginException, BankChoiceException {
@@ -91,7 +92,7 @@ public class Rikslunchen extends Bank {
         urlopen.setAllowCircularRedirects(true);
 
         List<NameValuePair> postData = new ArrayList<NameValuePair>();
-		postData.add(new BasicNameValuePair("c0-param0", "string:" + password));
+		postData.add(new BasicNameValuePair("c0-param0", "string:" + username));
 		postData.add(new BasicNameValuePair("callCount", "1"));
 		postData.add(new BasicNameValuePair("windowName", ""));
 		postData.add(new BasicNameValuePair("c0-scriptName", "cardUtil"));
@@ -138,7 +139,7 @@ public class Rikslunchen extends Bank {
 			LoginPackage lp = preLogin();
 
 			if (lp.getResponse().contains("Ange giltigt kortnummer.")) {
-				throw new LoginException(res.getText(R.string.invalid_username_password).toString());
+				throw new LoginException(res.getText(R.string.invalid_card_number).toString());
 			}
 
 			myResponse = lp.getResponse();
@@ -154,22 +155,46 @@ public class Rikslunchen extends Bank {
 	@Override
 	public void update() throws BankException, LoginException, BankChoiceException {
 		super.update();
-		if (password == null || password.length() == 0) {
-			throw new LoginException(res.getText(R.string.invalid_username_password).toString());
+		if (TextUtils.isEmpty(username)) {
+			throw new LoginException(res.getText(R.string.invalid_card_number).toString());
 		}
+        try {
+            urlopen = new Urllib(context);
+            urlopen.addHeader("Authorization", "basic Q0g6ODlAUHFqJGw4NyMjTVM=");
+            urlopen.addHeader("SOAPAction", "");
+            urlopen.addHeader("Content-Type", "text/xml;charset=UTF-8");
+            StringEntity body = new StringEntity(String.format("<v:Envelope xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:d=\"http://www.w3.org/2001/XMLSchema\" xmlns:c=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:v=\"http://schemas.xmlsoap.org/soap/envelope/\"><v:Header /><v:Body><n0:getBalance id=\"o0\" c:root=\"1\" xmlns:n0=\"urn:PhoneService\"><cardNo i:type=\"d:string\">%s</cardNo></n0:getBalance></v:Body></v:Envelope>", username), "UTF-8");
+            InputStream is = urlopen.openStream("https://www.rikslunchen.se/rkchws/PhoneService", body, true);
 
-		urlopen = login();
-		try {
+            Serializer serializer = new Persister();
+            Envelope resp = null;
+            try {
+                resp = serializer.read(Envelope.class, is, false);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-			int begin = myResponse.indexOf("balance");
-			int end = myResponse.indexOf("cardNo");
-			BigDecimal balance = Helpers.parseBalance(myResponse.substring(begin + 9, end - 2));
-
-			accounts.add(new Account("Rikslunchen", balance, "1"));
-		} catch (StringIndexOutOfBoundsException e) {
-			Log.e(TAG, "StringIndexOutOfBoundsException", e);
-		} finally {
-			super.updateComplete();
-		}
+            if (resp != null && resp.body != null && resp.body.fault != null && !TextUtils.isEmpty(resp.body.fault.faultstring)) {
+                throw new BankException(context.getString(R.string.invalid_card_number));
+                //faultString isn't always very descriptive
+                //throw new BankException(resp.body.fault.faultstring);
+            } else if (resp == null || resp.body == null || resp.body.getBalanceResponse == null || resp.body.getBalanceResponse.responseReturn == null || resp.body.getBalanceResponse.responseReturn.amount == null) {
+                throw new BankException(context.getString(R.string.invalid_card_number));
+            }
+            BigDecimal balance = Helpers.parseBalance(resp.body.getBalanceResponse.responseReturn.amount);
+            accounts.add(new Account("Rikslunchen", balance, "1"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (accounts.isEmpty()) {
+            throw new BankException(res.getText(R.string.no_accounts_found)
+                    .toString());
+        }
+		super.updateComplete();
 	}
+
 }
