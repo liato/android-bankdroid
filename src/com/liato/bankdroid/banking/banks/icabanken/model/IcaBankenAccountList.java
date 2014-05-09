123a1a5932204adc208419c1941cb0e985985b48
liato <x@x00.us>
2014-05-09 23:42:47 +0200

    Ignore unknown json properties for ICA Banken.

--- src/com/liato/bankdroid/banking/banks/icabanken/model/IcaBankenAccountList.java
+++ src/com/liato/bankdroid/banking/banks/icabanken/model/IcaBankenAccountList.java
@@ -3,118 +3,120 @@ package com.liato.bankdroid.banking.banks.icabanken.model;
 import java.math.BigDecimal;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class IcaBankenAccountList {
 
-	private List<IcaBankenAccount> mAccounts;
+    private List<IcaBankenAccount> mAccounts;
 
-	private String mDefaultAccountIdForEgiros;
-	private String mDefaultAccountIdForPayments;
-	private String mDefaultAccountIdForTransfers;
-	private BigDecimal mJointAccountsTotalAvailableAmount;
+    private String mDefaultAccountIdForEgiros;
+    private String mDefaultAccountIdForPayments;
+    private String mDefaultAccountIdForTransfers;
+    private BigDecimal mJointAccountsTotalAvailableAmount;
 
-	private BigDecimal mJointAccountsTotalCurrentAmount;
-	private BigDecimal mMinorsAccountsTotalAvailableAmount;
-	private BigDecimal mMinorsAccountsTotalCurrentAmount;
-	private BigDecimal mOwnAccountsTotalAvailableAmount;
-	private BigDecimal mOwnAccountsTotalCurrentAmount;
+    private BigDecimal mJointAccountsTotalCurrentAmount;
+    private BigDecimal mMinorsAccountsTotalAvailableAmount;
+    private BigDecimal mMinorsAccountsTotalCurrentAmount;
+    private BigDecimal mOwnAccountsTotalAvailableAmount;
+    private BigDecimal mOwnAccountsTotalCurrentAmount;
 
-	@JsonProperty("Accounts")
-	public List<IcaBankenAccount> getAccounts() {
-		return mAccounts;
-	}
+    @JsonProperty("Accounts")
+    public List<IcaBankenAccount> getAccounts() {
+        return mAccounts;
+    }
 
-	public void setAccounts(List<IcaBankenAccount> pAccounts) {
-		this.mAccounts = pAccounts;
-	}
+    public void setAccounts(List<IcaBankenAccount> pAccounts) {
+        this.mAccounts = pAccounts;
+    }
 
-	@JsonProperty("DefaultAccountIdForEgiros")
-	public String getDefaultAccountIdForEgiros() {
-		return mDefaultAccountIdForEgiros;
-	}
+    @JsonProperty("DefaultAccountIdForEgiros")
+    public String getDefaultAccountIdForEgiros() {
+        return mDefaultAccountIdForEgiros;
+    }
 
-	public void setDefaultAccountIdForEgiros(String pDefaultAccountIdForEgiros) {
-		this.mDefaultAccountIdForEgiros = pDefaultAccountIdForEgiros;
-	}
+    public void setDefaultAccountIdForEgiros(String pDefaultAccountIdForEgiros) {
+        this.mDefaultAccountIdForEgiros = pDefaultAccountIdForEgiros;
+    }
 
-	@JsonProperty("DefaultAccountIdForPayments")
-	public String getDefaultAccountIdForPayments() {
-		return mDefaultAccountIdForPayments;
-	}
+    @JsonProperty("DefaultAccountIdForPayments")
+    public String getDefaultAccountIdForPayments() {
+        return mDefaultAccountIdForPayments;
+    }
 
-	public void setDefaultAccountIdForPayments(
-			String pDefaultAccountIdForPayments) {
-		this.mDefaultAccountIdForPayments = pDefaultAccountIdForPayments;
-	}
+    public void setDefaultAccountIdForPayments(
+            String pDefaultAccountIdForPayments) {
+        this.mDefaultAccountIdForPayments = pDefaultAccountIdForPayments;
+    }
 
-	@JsonProperty("DefaultAccountIdForTransfers")
-	public String getDefaultAccountIdForTransfers() {
-		return mDefaultAccountIdForTransfers;
-	}
+    @JsonProperty("DefaultAccountIdForTransfers")
+    public String getDefaultAccountIdForTransfers() {
+        return mDefaultAccountIdForTransfers;
+    }
 
-	public void setDefaultAccountIdForTransfers(
-			String pDefaultAccountIdForTransfers) {
-		this.mDefaultAccountIdForTransfers = pDefaultAccountIdForTransfers;
-	}
+    public void setDefaultAccountIdForTransfers(
+            String pDefaultAccountIdForTransfers) {
+        this.mDefaultAccountIdForTransfers = pDefaultAccountIdForTransfers;
+    }
 
-	@JsonProperty("JointAccountsTotalAvailableAmount")
-	public BigDecimal getJointAccountsTotalAvailableAmount() {
-		return mJointAccountsTotalAvailableAmount;
-	}
+    @JsonProperty("JointAccountsTotalAvailableAmount")
+    public BigDecimal getJointAccountsTotalAvailableAmount() {
+        return mJointAccountsTotalAvailableAmount;
+    }
 
-	public void setJointAccountsTotalAvailableAmount(
-			BigDecimal pJointAccountsTotalAvailableAmount) {
-		this.mJointAccountsTotalAvailableAmount = pJointAccountsTotalAvailableAmount;
-	}
+    public void setJointAccountsTotalAvailableAmount(
+            BigDecimal pJointAccountsTotalAvailableAmount) {
+        this.mJointAccountsTotalAvailableAmount = pJointAccountsTotalAvailableAmount;
+    }
 
-	@JsonProperty("JointAccountsTotalCurrentAmount")
-	public BigDecimal getJointAccountsTotalCurrentAmount() {
-		return mJointAccountsTotalCurrentAmount;
-	}
+    @JsonProperty("JointAccountsTotalCurrentAmount")
+    public BigDecimal getJointAccountsTotalCurrentAmount() {
+        return mJointAccountsTotalCurrentAmount;
+    }
 
-	public void setJointAccountsTotalCurrentAmount(
-			BigDecimal pJointAccountsTotalCurrentAmount) {
-		this.mJointAccountsTotalCurrentAmount = pJointAccountsTotalCurrentAmount;
-	}
+    public void setJointAccountsTotalCurrentAmount(
+            BigDecimal pJointAccountsTotalCurrentAmount) {
+        this.mJointAccountsTotalCurrentAmount = pJointAccountsTotalCurrentAmount;
+    }
 
-	@JsonProperty("MinorsAccountsTotalAvailableAmount")
-	public BigDecimal getMinorsAccountsTotalAvailableAmount() {
-		return mMinorsAccountsTotalAvailableAmount;
-	}
+    @JsonProperty("MinorsAccountsTotalAvailableAmount")
+    public BigDecimal getMinorsAccountsTotalAvailableAmount() {
+        return mMinorsAccountsTotalAvailableAmount;
+    }
 
-	public void setMinorsAccountsTotalAvailableAmount(
-			BigDecimal pMinorsAccountsTotalAvailableAmount) {
-		this.mMinorsAccountsTotalAvailableAmount = pMinorsAccountsTotalAvailableAmount;
-	}
+    public void setMinorsAccountsTotalAvailableAmount(
+            BigDecimal pMinorsAccountsTotalAvailableAmount) {
+        this.mMinorsAccountsTotalAvailableAmount = pMinorsAccountsTotalAvailableAmount;
+    }
 
-	@JsonProperty("MinorsAccountsTotalCurrentAmount")
-	public BigDecimal getMinorsAccountsTotalCurrentAmount() {
-		return mMinorsAccountsTotalCurrentAmount;
-	}
+    @JsonProperty("MinorsAccountsTotalCurrentAmount")
+    public BigDecimal getMinorsAccountsTotalCurrentAmount() {
+        return mMinorsAccountsTotalCurrentAmount;
+    }
 
-	public void setMinorsAccountsTotalCurrentAmount(
-			BigDecimal pMinorsAccountsTotalCurrentAmount) {
-		this.mMinorsAccountsTotalCurrentAmount = pMinorsAccountsTotalCurrentAmount;
-	}
+    public void setMinorsAccountsTotalCurrentAmount(
+            BigDecimal pMinorsAccountsTotalCurrentAmount) {
+        this.mMinorsAccountsTotalCurrentAmount = pMinorsAccountsTotalCurrentAmount;
+    }
 
-	@JsonProperty("OwnAccountsTotalAvailableAmount")
-	public BigDecimal getOwnAccountsTotalAvailableAmount() {
-		return mOwnAccountsTotalAvailableAmount;
-	}
+    @JsonProperty("OwnAccountsTotalAvailableAmount")
+    public BigDecimal getOwnAccountsTotalAvailableAmount() {
+        return mOwnAccountsTotalAvailableAmount;
+    }
 
-	public void setOwnAccountsTotalAvailableAmount(
-			BigDecimal pOwnAccountsTotalAvailableAmount) {
-		this.mOwnAccountsTotalAvailableAmount = pOwnAccountsTotalAvailableAmount;
-	}
+    public void setOwnAccountsTotalAvailableAmount(
+            BigDecimal pOwnAccountsTotalAvailableAmount) {
+        this.mOwnAccountsTotalAvailableAmount = pOwnAccountsTotalAvailableAmount;
+    }
 
-	@JsonProperty("OwnAccountsTotalCurrentAmount")
-	public BigDecimal getOwnAccountsTotalCurrentAmount() {
-		return mOwnAccountsTotalCurrentAmount;
-	}
+    @JsonProperty("OwnAccountsTotalCurrentAmount")
+    public BigDecimal getOwnAccountsTotalCurrentAmount() {
+        return mOwnAccountsTotalCurrentAmount;
+    }
 
-	public void setOwnAccountsTotalCurrentAmount(
-			BigDecimal pOwnAccountsTotalCurrentAmount) {
-		this.mOwnAccountsTotalCurrentAmount = pOwnAccountsTotalCurrentAmount;
-	}
+    public void setOwnAccountsTotalCurrentAmount(
+            BigDecimal pOwnAccountsTotalCurrentAmount) {
+        this.mOwnAccountsTotalCurrentAmount = pOwnAccountsTotalCurrentAmount;
+    }
 }
--- src/com/liato/bankdroid/banking/banks/icabanken/model/IcaBankenTransaction.java
+++ src/com/liato/bankdroid/banking/banks/icabanken/model/IcaBankenTransaction.java
@@ -3,51 +3,53 @@ package com.liato.bankdroid.banking.banks.icabanken.model;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class IcaBankenTransaction {
 
-	private BigDecimal mAccountBalance;
+    private BigDecimal mAccountBalance;
 
-	private BigDecimal mAmount;
+    private BigDecimal mAmount;
 
-	private String mMemoText;
+    private String mMemoText;
 
-	private Date mPostedDate;
+    private Date mPostedDate;
 
-	@JsonProperty("AccountBalance")
-	public BigDecimal getAccountBalance() {
-		return mAccountBalance;
-	}
+    @JsonProperty("AccountBalance")
+    public BigDecimal getAccountBalance() {
+        return mAccountBalance;
+    }
 
-	public void setAccountBalance(BigDecimal pAccountBalance) {
-		this.mAccountBalance = pAccountBalance;
-	}
+    public void setAccountBalance(BigDecimal pAccountBalance) {
+        this.mAccountBalance = pAccountBalance;
+    }
 
-	@JsonProperty("Amount")
-	public BigDecimal getAmount() {
-		return mAmount;
-	}
+    @JsonProperty("Amount")
+    public BigDecimal getAmount() {
+        return mAmount;
+    }
 
-	public void setAmount(BigDecimal pAmount) {
-		this.mAmount = pAmount;
-	}
+    public void setAmount(BigDecimal pAmount) {
+        this.mAmount = pAmount;
+    }
 
-	@JsonProperty("MemoText")
-	public String getMemoText() {
-		return mMemoText;
-	}
+    @JsonProperty("MemoText")
+    public String getMemoText() {
+        return mMemoText;
+    }
 
-	public void setMemoText(String pMemoText) {
-		this.mMemoText = pMemoText;
-	}
+    public void setMemoText(String pMemoText) {
+        this.mMemoText = pMemoText;
+    }
 
-	@JsonProperty("PostedDate")
-	public Date getPostedDate() {
-		return mPostedDate;
-	}
+    @JsonProperty("PostedDate")
+    public Date getPostedDate() {
+        return mPostedDate;
+    }
 
-	public void setPostedDate(Date pPostedDate) {
-		this.mPostedDate = pPostedDate;
-	}
+    public void setPostedDate(Date pPostedDate) {
+        this.mPostedDate = pPostedDate;
+    }
 }
