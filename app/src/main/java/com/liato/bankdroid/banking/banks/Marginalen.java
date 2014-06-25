8764a81d940ed809819bc131b7660d216cb8f039
liato <x@x00.us>
2014-06-26 00:30:41 +0200

    Update certificate for Eurocard, Ikano Bank, Marginalen Bank, Nordnet. Nordnet Direkt, Payson and Trustbuddy.

--- app/src/main/java/com/liato/bankdroid/banking/banks/Marginalen.java
+++ app/src/main/java/com/liato/bankdroid/banking/banks/Marginalen.java
@@ -65,7 +65,7 @@ public class Marginalen extends Bank {
     @Override
     protected LoginPackage preLogin() throws BankException,
             ClientProtocolException, IOException {
-        urlopen = new Urllib(context, CertificateReader.getCertificates(context, R.raw.cert_marginalen));
+        urlopen = new Urllib(context, CertificateReader.getCertificates(context, R.raw.cert_marginalen, R.raw.cert_marginalen2));
         urlopen.setContentCharset(HTTP.ISO_8859_1);
         Matcher matcher;
         response = urlopen.open(BASE_URL + "engine");
