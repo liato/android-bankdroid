8708e7904bd6318fa9782ec47d046b01006c30d8
Fredrik Erlandsson <fredrik.e@gmail.com>
2015-01-26 14:20:39 +0100

    Added transactions to "Poäng" account at Coop

--- app/src/main/java/com/liato/bankdroid/banking/banks/coop/Coop.java
+++ app/src/main/java/com/liato/bankdroid/banking/banks/coop/Coop.java
@@ -229,9 +229,18 @@ public class Coop extends Bank {
 
         try {
             response = urlopen.open("https://www.coop.se/Mina-sidor/Oversikt/Mina-poang/");
+            Document dResponse = Jsoup.parse(response);
             Account poang = new Account("Poäng",
-                    Helpers.parseBalance(Jsoup.parse(response).select(".Grid-cell--1 p").text()),
+                    Helpers.parseBalance(dResponse.select(".Grid-cell--1 p").text()),
                     "poang", Account.OTHER, "");
+            List<Transaction> transactions = new ArrayList<Transaction>();
+            poang.setTransactions(transactions);
+            for (Element e : dResponse.select(".Timeline-item")) {
+                transactions.add(new Transaction(
+                    formatDate(e.select(".Timeline-header .u-nbfcAlt span").text()),
+                    e.select(".u-block").text(),
+                    Helpers.parseBalance(e.select(".Timeline-header .Timeline-title").first().ownText()), ""));
+            }
             accounts.add(poang);
             for (AccountType at : AccountType.values()) {
                 response = urlopen.open(at.getUrl());
