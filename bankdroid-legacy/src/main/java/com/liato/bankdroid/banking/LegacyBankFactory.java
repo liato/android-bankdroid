2ffc769ce796f9f811f6d0951c4dfaaece0ede49
Mathias Åhsberg <mathias.ahsberg@gmail.com>
2015-02-22 21:17:19 +0100

    Moves fromBanktypeId method to LegacyBankFactory.

--- bankdroid-legacy/src/main/java/com/liato/bankdroid/banking/LegacyBankFactory.java
+++ bankdroid-legacy/src/main/java/com/liato/bankdroid/banking/LegacyBankFactory.java
@@ -17,6 +17,7 @@ import com.liato.bankdroid.banking.banks.BrummerKF;
 import com.liato.bankdroid.banking.banks.CSN;
 import com.liato.bankdroid.banking.banks.Chalmrest;
 import com.liato.bankdroid.banking.banks.Chevrolet;
+import com.liato.bankdroid.banking.banks.DanskeBank;
 import com.liato.bankdroid.banking.banks.DinersClub;
 import com.liato.bankdroid.banking.banks.Djurgarden;
 import com.liato.bankdroid.banking.banks.EasyCard;
@@ -54,6 +55,7 @@ import com.liato.bankdroid.banking.banks.SevenDay;
 import com.liato.bankdroid.banking.banks.Shell;
 import com.liato.bankdroid.banking.banks.Skandiabanken;
 import com.liato.bankdroid.banking.banks.Skoda;
+import com.liato.bankdroid.banking.banks.SparbankenOresund;
 import com.liato.bankdroid.banking.banks.SparbankenSyd;
 import com.liato.bankdroid.banking.banks.Statoil;
 import com.liato.bankdroid.banking.banks.SupremeCard;
@@ -80,11 +82,177 @@ import com.liato.bankdroid.banking.banks.rikslunchen.Rikslunchen;
 import com.liato.bankdroid.banking.banks.seb.SEB;
 import com.liato.bankdroid.banking.banks.swedbank.Sparbankerna;
 import com.liato.bankdroid.banking.banks.swedbank.Swedbank;
+import com.liato.bankdroid.banking.exceptions.BankException;
+import com.liato.bankdroid.provider.IBankTypes;
 
 import java.util.ArrayList;
 
 public class LegacyBankFactory {
 
+    public static Bank fromBanktypeId(int id, Context context) throws BankException {
+        switch (id) {
+            case IBankTypes.TESTBANK:
+                return new TestBank(context);
+            case IBankTypes.AKELIUSINVEST:
+                return new AkeliusInvest(context);
+            case IBankTypes.AKELIUSSPAR:
+                return new AkeliusSpar(context);
+            case IBankTypes.SWEDBANK:
+                return new Swedbank(context);
+            case IBankTypes.NORDEA:
+                return new Nordea(context);
+            case IBankTypes.LANSFORSAKRINGAR:
+                return new Lansforsakringar(context);
+            case IBankTypes.ICABANKEN:
+                return new ICABanken(context);
+            case IBankTypes.HANDELSBANKEN:
+                return new Handelsbanken(context);
+            case IBankTypes.COOP:
+                return new Coop(context);
+            case IBankTypes.ICA:
+                return new ICA(context);
+            case IBankTypes.STATOIL:
+                return new Statoil(context);
+            case IBankTypes.AVANZA:
+                return new Avanza(context);
+            case IBankTypes.VILLABANKEN:
+                return new Villabanken(context);
+            case IBankTypes.AVANZAMINI:
+                return new AvanzaMini(context);
+            case IBankTypes.OKQ8:
+                return new OKQ8(context);
+            case IBankTypes.EUROCARD:
+                return new Eurocard(context);
+            case IBankTypes.FIRSTCARD:
+                return new FirstCard(context);
+            case IBankTypes.PAYPAL:
+                return new PayPal(context);
+            case IBankTypes.PAYSON:
+                return new Payson(context);
+            case IBankTypes.JOJO:
+                return new Jojo(context);
+            //case IBankTypes.STEAM:
+            //    return new Steam(context);
+            case IBankTypes.DINERSCLUB:
+                return new DinersClub(context);
+            case IBankTypes.IKANOBANK:
+                return new IkanoBank(context);
+            case IBankTypes.SASEUROBONUSMASTERCARD:
+                return new EurobonusMastercard(context);
+            case IBankTypes.SASEUROBONUSMASTERCARD_NO:
+                return new EurobonusMastercardNo(context);
+            case IBankTypes.SASEUROBONUSMASTERCARD_DK:
+                return new EurobonusMastercardDk(context);
+            case IBankTypes.RIKSLUNCHEN:
+                return new Rikslunchen(context);
+            case IBankTypes.HEMKOP:
+                return new Hemkop(context);
+            case IBankTypes.SEB:
+                return new SEB(context);
+            case IBankTypes.NORDNET:
+                return new Nordnet(context);
+            case IBankTypes.SEVENDAY:
+                return new SevenDay(context);
+            case IBankTypes.OSUUSPANKKI:
+                return new Osuuspankki(context);
+            case IBankTypes.VOLVOFINANS:
+                return new Volvofinans(context);
+            case IBankTypes.CSN:
+                return new CSN(context);
+            case IBankTypes.RESURSBANK:
+                return new ResursBank(context);
+            case IBankTypes.AMERICANEXPRESS:
+                return new AmericanExpress(context);
+            case IBankTypes.MCDONALDS:
+                return new McDonalds(context);
+            case IBankTypes.SAAB:
+                return new Saab(context);
+            case IBankTypes.WALLET:
+                return new Wallet(context);
+            case IBankTypes.CHEVROLET:
+                return new Chevrolet(context);
+            case IBankTypes.SJPRIO:
+                return new SJPrio(context);
+            case IBankTypes.OPEL:
+                return new Opel(context);
+            case IBankTypes.DJURGARDEN:
+                return new Djurgarden(context);
+            case IBankTypes.QUINTESSENTIALLY:
+                return new Quintessentially(context);
+            case IBankTypes.PLUSGIROT:
+                return new PlusGirot(context);
+            case IBankTypes.SHELL:
+                return new Shell(context);
+            case IBankTypes.VOLKSWAGEN:
+                return new Volkswagen(context);
+            case IBankTypes.AUDI:
+                return new Audi(context);
+            case IBankTypes.PREEM:
+                return new Preem(context);
+            case IBankTypes.SEAT:
+                return new Seat(context);
+            case IBankTypes.SKODA:
+                return new Skoda(context);
+            case IBankTypes.IKEA:
+                return new IKEA(context);
+            case IBankTypes.SPARBANKEN_SYD:
+                return new SparbankenSyd(context);
+            case IBankTypes.SPARBANKEN_ORESUND:
+                return new SparbankenOresund(context);
+            case IBankTypes.NORDNETDIREKT:
+                return new Nordnetdirekt(context);
+            case IBankTypes.SKANDIABANKEN:
+                return new Skandiabanken(context);
+            case IBankTypes.DANSKEBANK:
+                return new DanskeBank(context);
+            case IBankTypes.NORDEA_DK:
+                return new NordeaDK(context);
+            case IBankTypes.VASTTRAFIK:
+                return new Vasttrafik(context);
+            case IBankTypes.EVERYDAYCARD:
+                return new Everydaycard(context);
+            case IBankTypes.MENIGA:
+                return new Meniga(context);
+            case IBankTypes.RIKSKORTET:
+                return new TicketRikskortet(context);
+            case IBankTypes.BIOKLUBBEN:
+                return new Bioklubben(context);
+            case IBankTypes.CHALMREST:
+                return new Chalmrest(context);
+            case IBankTypes.MARGINALEN:
+                return new Marginalen(context);
+            case IBankTypes.SVENSKASPEL:
+                return new SvenskaSpel(context);
+            case IBankTypes.EASYCARD:
+                return new EasyCard(context);
+            case IBankTypes.APPEAKPOKER:
+                return new AppeakPoker(context);
+            case IBankTypes.TRUSTBUDDY:
+                return new TrustBuddy(context);
+            case IBankTypes.BRUMMER_KF:
+                return new BrummerKF(context);
+            case IBankTypes.ZIDISHA:
+                return new Zidisha(context);
+            case IBankTypes.BETTERGLOBE:
+                return new BetterGlobe(context);
+            case IBankTypes.FOREX:
+                return new ForexBank(context);
+            case IBankTypes.BITCOIN:
+                return new Bitcoin(context);
+            case IBankTypes.SVEADIREKT:
+                return new SveaDirekt(context);
+            case IBankTypes.SUPREMECARD:
+                return new SupremeCard(context);
+            case IBankTypes.SPARBANKERNA:
+                return new Sparbankerna(context);
+            case IBankTypes.BLEKINGETRAFIKEN:
+                return new BlekingeTrafiken(context);
+            case IBankTypes.OSTGOTATRAFIKEN:
+                return new Ostgotatrafiken(context);
+            default:
+                throw new BankException("BankType id not found.");
+        }
+    }
 
     public static ArrayList<Bank> listBanks(Context context) {
         ArrayList<Bank> banks = new ArrayList<Bank>();
