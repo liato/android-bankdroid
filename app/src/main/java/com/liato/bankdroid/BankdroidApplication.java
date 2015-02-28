a317e5923fc8739257b44d61ccdbb9cc41556c12
Mathias Åhsberg <mathias.ahsberg@gmail.com>
2015-02-28 16:20:15 +0100

    Adds default locale setting to crash reports.

--- app/src/main/java/com/liato/bankdroid/BankdroidApplication.java
+++ app/src/main/java/com/liato/bankdroid/BankdroidApplication.java
@@ -4,13 +4,18 @@ import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
 
+import java.util.Locale;
+
 import io.fabric.sdk.android.Fabric;
 
 public class BankdroidApplication extends Application {
+
+    public static final String LOG_KEY_LOCALE = "locale";
 
     @Override
     public void onCreate() {
         super.onCreate();
         Fabric.with(this, new Crashlytics());
+        Crashlytics.setString(LOG_KEY_LOCALE, Locale.getDefault().toString());
     }
 }
