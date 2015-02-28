21aa57e5d0af79194b604d29639ca46e883c32ae
Mathias Åhsberg <mathias.ahsberg@gmail.com>
2015-02-28 16:45:49 +0100

    Adds bank names to Crashlytics logging.

--- app/src/main/java/com/liato/bankdroid/BankdroidApplication.java
+++ app/src/main/java/com/liato/bankdroid/BankdroidApplication.java
@@ -3,7 +3,10 @@ package com.liato.bankdroid;
 import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
+import com.liato.bankdroid.banking.Bank;
+import com.liato.bankdroid.banking.BankFactory;
 
+import java.util.List;
 import java.util.Locale;
 
 import io.fabric.sdk.android.Fabric;
@@ -11,11 +14,25 @@ import io.fabric.sdk.android.Fabric;
 public class BankdroidApplication extends Application {
 
     public static final String LOG_KEY_LOCALE = "locale";
+    public static final String LOG_KEY_BANKS = "banks";
 
     @Override
     public void onCreate() {
         super.onCreate();
         Fabric.with(this, new Crashlytics());
         Crashlytics.setString(LOG_KEY_LOCALE, Locale.getDefault().toString());
+        logBanks();
+    }
+
+    private void logBanks() {
+        List<Bank> banks = BankFactory.banksFromDb(this, false);
+        StringBuilder bankStringBuilder = new StringBuilder();
+        if (banks != null && !banks.isEmpty()) {
+            for (Bank bank : banks) {
+                bankStringBuilder.append(bank.getName())
+                        .append(",");
+            }
+            Crashlytics.setString(LOG_KEY_BANKS, bankStringBuilder.substring(0, bankStringBuilder.length() - 1));
+        }
     }
 }
