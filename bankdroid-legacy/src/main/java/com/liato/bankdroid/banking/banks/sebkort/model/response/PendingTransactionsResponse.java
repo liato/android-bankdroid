3991e4d647cf9b97923dc622230d0939b68d1d63
Mathias Åhsberg <mathias.ahsberg@gmail.com>
2016-10-17 21:49:54 +0200

    Remove unused banks

--- bankdroid-legacy/src/main/java/com/liato/bankdroid/banking/banks/sebkort/model/response/PendingTransactionsResponse.java
+++
@@ -1,8 +0,0 @@
-package com.liato.bankdroid.banking.banks.sebkort.model.response;
-
-import com.liato.bankdroid.banking.banks.sebkort.model.PendingTransactions;
-
-public class PendingTransactionsResponse extends BaseResponse<PendingTransactions> {
-
-    private static final long serialVersionUID = 3753708966368822328L;
-}
