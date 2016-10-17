26cfc388679d968e7f745c36af31448057c7c879
Mathias Åhsberg <mathias.ahsberg@gmail.com>
2016-10-17 21:08:26 +0200

    Make sure that transaction model for amex never return null, but an empty list if no transactions are available.
    Fixes #637

--- bankdroid-legacy/src/main/java/com/liato/bankdroid/banking/banks/americanexpress/model/AccountActivity.java
+++ bankdroid-legacy/src/main/java/com/liato/bankdroid/banking/banks/americanexpress/model/AccountActivity.java
@@ -2,6 +2,7 @@ package com.liato.bankdroid.banking.banks.americanexpress.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.Collections;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -9,7 +10,8 @@ public class AccountActivity {
     List<Transaction> transactionList;
 
     public List<Transaction> getTransactionList() {
-        return transactionList;
+        return transactionList == null ?
+                Collections.<Transaction>emptyList() : transactionList;
     }
 
     public void setTransactionList(List<Transaction> transactionList) {
